b4716649bf68edc877229f63aa84b54ed05a7527
clannad <xxi124@sina.com>
2017-10-19 18:12:06 +0800

    1
    1

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import VueResource from 'vue-resource'
+import Goods from '@/components/goods/goods'
+import Seller from '@/components/seller/seller'
+import Ratings from '@/components/ratings/ratings'
+Vue.use(Router)
+Vue.use(VueResource)
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'goods',
+      component: Goods
+    },
+    {
+      path: '/seller',
+      name: 'seller',
+      component: Seller
+    },
+    {
+      path: '/ratings',
+      name: 'ratings',
+      component: Ratings
+    }
+  ]
+})
