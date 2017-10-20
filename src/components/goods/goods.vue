e159875ad8c42ee171864bb331f6e705ae1da2c4
clannad <xxi124@sina.com>
2017-10-20 16:56:18 +0800

    2
    2

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,12 +1,215 @@
 <template>
   <div>
-    我是goods
+    <div class="goods">
+      <div class="menu-wrapper" ref="menuWrapper">
+        <ul>
+          <li v-for="( item, index ) in goods" class="menu-item" :class="{'current' : currentIndex === index }"
+              @click="selectMenu(index)" ref="menuList">
+            <span class="text border-1px">
+              <span v-show="item.type>0" class="icon">1</span>
+              {{item.name}}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="food-wrapper" ref="foodsWrapper">
+        <ul>
+          <li v-for="item in goods" class="food-list food-list-hook">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="food in item.foods" class="food-item border-1px">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon" alt="">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">¥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-  export default {}
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+
+  const ERR_OK = 0
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: [],
+        listHeight: [],
+        scrollY: 0
+      }
+    },
+    computed: {
+      currentIndex () {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            this._followScroll(i)
+            return i
+          }
+        }
+        return 0
+      }
+    },
+    created () {
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.goods = response.data
+          this.$nextTick(() => {
+            this._initScroll()
+            this._calculateHeight()
+          })
+        }
+      })
+    },
+    methods: {
+      selectMenu (index) {
+        console.log(index)
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+        let el = foodList[index]
+        this.foodsScroll.scrollToElement(el, 300)
+      },
+      _initScroll () {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        })
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          probeType: 3
+        })
+
+        this.foodsScroll.on('scroll', (pos) => {
+          // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
+          if (pos.y <= 0) {
+            this.scrollY = Math.abs(Math.round(pos.y))
+          }
+        })
+      },
+      _calculateHeight () {
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook')
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+//          console.log(this.listHeight)
+        }
+      },
+      _followScroll (index) {
+        let menuList = this.$refs.menuList
+        let el = menuList[index]
+        this.menuScroll.scrollToElement(el, 300, 0, -100)
+      }
+    }
+  }
 </script>
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import '../../assets/stylus/mixin'
+
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px //安卓兼容
+      background-color: #f3f5f7
+      .menu-item //class的级联速度快
+        display: table
+        width: 56px
+        height: 54px
+        line-height: 14px
+        padding: 0 12px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background: #fff
+          font-weight: 700
+          .text
+            border-none()
+        .text
+          display: table-cell
+          width: 56px
+          text-align: center
+          vertical-align: middle
+          font-size: 12px
+          border-1px(rgba(7, 17, 27, 0.1))
+    .food-wrapper
+      flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background-color: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-clild
+          border-none()
+          margin-bottom: 0px
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            line-height: 12px
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
 
 </style>
