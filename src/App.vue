f199ab50b526debba7a056f8248e6124c14a4525
clannad <xxi124@sina.com>
2017-10-21 16:51:52 +0800

    10.21

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller" ></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/">商品</router-link>
