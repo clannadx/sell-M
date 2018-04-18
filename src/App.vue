8a743f4386b3b7864b59b5f949a9ea34e4183b5e
clannadx <xxi124@sina.com>
2018-04-18 17:41:56 +0800

    4.2

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,13 @@
       </div>
     </div>
     <router-view :seller="seller"/>
+    <loading :seller="seller" :loading="loading" :resultCode="resultCode"></loading>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import header from '@/components/header/header'
+  import loading from '@/components/loading/loading'
   import {urlParse} from '@/assets/js/util'
   const ERR_OK = 0
   const debug = process.env.NODE_ENV !== 'production'
@@ -30,16 +32,22 @@
             let queryParam = urlParse()
             return queryParam.id
           })()
+        },
+        loading: false,
+        resultCode: {
+          type: Number
         }
       }
     },
     components: {
-      'v-header': header
+      'v-header': header,
+      loading
     },
     created () {
       const url = debug ? '/api/seller' : 'https://easy-mock.com/mock/59c9aed5e0dc663341ba41de/example_1506389717568/seller'
       this.$http.get(url + '?id=' + this.seller.id).then((response) => {
         response = response.body
+        this.resultCode = response.status
         if (response.errno === ERR_OK) {
           this.seller = Object.assign({}, this.seller, response.data)    // 给对象扩展属性
           console.log(this.seller.id)
