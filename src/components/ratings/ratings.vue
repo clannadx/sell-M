d00a55f647131f9909157678f01938a3daf86383
clannadx <xxi124@sina.com>
2018-07-26 20:59:59 +0800

    '修改接口'

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -90,7 +90,7 @@
       loading
     },
     created () {
-      const url = debug ? '/api/ratings' : 'https://easy-mock.com/mock/59c9aed5e0dc663341ba41de/example_1506389717568/ratings'
+      const url = debug ? '/api/ratings' : 'https://easy-mock.com/mock/59c9aed5e0dc663341ba41de/web/ratings'
       this.$http.get(url).then((response) => {
         response = response.body
         this.resultCode = response.status
